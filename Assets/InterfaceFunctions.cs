5a456ae6be36b45864d038578c71a7d8c8a3938c
Silvie Mueller <silvie.mueller@web.de>
2020-09-25 11:29:03 +0200

    add slider to change Opacity of Gameobjects

---
+++ Assets/InterfaceFunctions.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InterfaceFunctions : MonoBehaviour
+{
+    Slider mainSlider;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainSlider = GetComponentInChildren<Slider>();
+        mainSlider.onValueChanged.AddListener(delegate { OnSliderWasChanged(); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnSliderWasChanged()
+    {
+        //Debug.Log(mainSlider.value);
+        OnMeshClick[] components = GameObject.FindObjectsOfType<OnMeshClick>();
+        foreach (var onMeshClick in components)
+        {
+            var meshRenderers = onMeshClick.transform.GetComponentsInChildren<MeshRenderer>();
+            foreach (var meshRenderer in meshRenderers)
+            {
+                meshRenderer.material.SetFloat("Vector1", mainSlider.value);
+                meshRenderer.material.SetFloat("Alpha", mainSlider.value);
+            }
+        }
+    }
+
+}

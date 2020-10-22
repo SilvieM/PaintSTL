192a9999ee421803c222f4f407a365ce523c6860
Silvie Mueller <silvie.mueller@web.de>
2020-10-22 13:09:55 +0200

    add lib

--- Assets/Import.cs
+++ Assets/Import.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Assets.g3UnityUtils;
+using g3;
 using Parabox.Stl;
 using UnityEditor;
 using UnityEngine;
@@ -34,54 +36,63 @@ public class Import : MonoBehaviour
         }
 
         var name = Path.GetFileNameWithoutExtension(path);
-        var meshes = Importer.Import(path, CoordinateSpace.Right).ToArray(); //TODO Check Coordinate Space
+        DMesh3 readMesh = StandardMeshReader.ReadMesh(path);
 
-        if (meshes.Length < 1)
-            return;
 
-        if (meshes.Length < 2)
-        {
-            var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            Object.DestroyImmediate(go.GetComponent<BoxCollider>());
-            go.name = name;
-            meshes[0].name = "0";
-            go.GetComponent<MeshFilter>().sharedMesh = meshes[0];
-            var res = Resources.Load("STLMeshMaterial2") as Material;
-            go.GetComponent<MeshRenderer>().material = res;
-            go.AddComponent<OnMeshClick>().Start();
-            go.AddComponent<MeshCollider>();
-            go.AddComponent<Generate>().GenerateMesh();
-            go.transform.position = Vector3.zero;
-            go.transform.localScale = new Vector3(0.1f,0.1f,0.1f);
-            created = go;
-        }
-        else
-        {
-            var parent = new GameObject();
-            parent.name = name;
-            for (int i = 0, c = meshes.Length; i < c; i++)
-            {
-                var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                Object.DestroyImmediate(go.GetComponent<BoxCollider>());
-                go.transform.SetParent(parent.transform, false);
-                go.name = name + "(" + i + ")";
+        var obj = g3UnityUtils.CreateMeshGO("ImportedObject", readMesh, Color.white);
+        
+        Instantiate(obj);
 
-                var mesh = meshes[i];
-                mesh.name = i.ToString();
-                go.GetComponent<MeshFilter>().sharedMesh = mesh;
-                var res = Resources.Load("STLMeshMaterial2") as Material;
-                go.GetComponent<MeshRenderer>().material = res;
-                go.AddComponent<MeshCollider>();
-                go.transform.position = Vector3.zero;
-            }
-            parent.AddComponent<OnMeshClick>().Start();
-            parent.AddComponent<Generate>().GenerateMesh();
-            parent.transform.position = Vector3.zero;
-            parent.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
-            created = parent;
-        }
-        var cam = GameObject.FindGameObjectWithTag("MainCamera"); //TODO make this work
-        cam.transform.LookAt(created.transform);
+
+
+        //var meshes = Importer.Import(path, CoordinateSpace.Right).ToArray(); //TODO Check Coordinate Space
+
+        //if (meshes.Length < 1)
+        //    return;
+
+        //if (meshes.Length < 2)
+        //{
+        //    var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //    Object.DestroyImmediate(go.GetComponent<BoxCollider>());
+        //    go.name = name;
+        //    meshes[0].name = "0";
+        //    go.GetComponent<MeshFilter>().sharedMesh = meshes[0];
+        //    var res = Resources.Load("STLMeshMaterial2") as Material;
+        //    go.GetComponent<MeshRenderer>().material = res;
+        //    go.AddComponent<OnMeshClick>().Start();
+        //    go.AddComponent<MeshCollider>();
+        //    go.AddComponent<Generate>().GenerateMesh();
+        //    go.transform.position = Vector3.zero;
+        //    go.transform.localScale = new Vector3(0.1f,0.1f,0.1f);
+        //    created = go;
+        //}
+        //else
+        //{
+        //    var parent = new GameObject();
+        //    parent.name = name;
+        //    for (int i = 0, c = meshes.Length; i < c; i++)
+        //    {
+        //        var go = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        //        Object.DestroyImmediate(go.GetComponent<BoxCollider>());
+        //        go.transform.SetParent(parent.transform, false);
+        //        go.name = name + "(" + i + ")";
+
+        //        var mesh = meshes[i];
+        //        mesh.name = i.ToString();
+        //        go.GetComponent<MeshFilter>().sharedMesh = mesh;
+        //        var res = Resources.Load("STLMeshMaterial2") as Material;
+        //        go.GetComponent<MeshRenderer>().material = res;
+        //        go.AddComponent<MeshCollider>();
+        //        go.transform.position = Vector3.zero;
+        //    }
+        //    parent.AddComponent<OnMeshClick>().Start();
+        //    parent.AddComponent<Generate>().GenerateMesh();
+        //    parent.transform.position = Vector3.zero;
+        //    parent.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+        //    created = parent;
+        //}
+        //var cam = GameObject.FindGameObjectWithTag("MainCamera"); //TODO make this work
+        //cam.transform.LookAt(created.transform);
         
     }
 }
